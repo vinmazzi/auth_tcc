b3e90485bdd2b8759e93368e2e38133dc47f2c87
Vinicius Mazzi <vmazzi@vmazzi.localdomain>
2015-08-31 20:42:21 -0300

    Adicao de regra para liberar o ip dos clientes

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,9 +1,5 @@
 class LoginsController < ApplicationController
    def new
-	response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
-        response.headers["Pragma"] = "no-cache"
-        response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
-
    end
 
    def create 
@@ -11,9 +7,9 @@ class LoginsController < ApplicationController
 	login = teste["login"]
 	senha = teste["pwd"]
 	ip = request.remote_ip
-	if(login == "teste" && senha == "vai")
-		system("iptables -t nat -D PREROUTING 1")
-		system("iptables -t nat -D PREROUTING 1")
+	if(login == "vmazzi" && senha == "teste")
+		system("iptables -t nat -I AUTH_TESTE -s #{ip}/32 -p tcp -j ACCEPT")
+		#system("iptables -t nat -D PREROUTING 1")
 		redirect_to :back
 	end
 	
