6fcefeadafa2e602f72b338d262dc8d12a8f2e05
Vinicius Mazzi <vmazzi@vmazzi.localdomain>
2015-08-31 20:52:25 -0300

    Redirect de todas urls para forms#new

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
    
   resources :logins
   root 'logins#new'
+  get '*path', to: 'logins#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
